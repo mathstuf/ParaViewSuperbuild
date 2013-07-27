07aff89105e451c9d119749241a050ac4575d133
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2013-07-27 18:14:28 -0400

    netCDF4 on windows requires a cmakelists patch

--- Projects/netcdf4.cmake
+++ Projects/netcdf4.cmake
@@ -2,9 +2,11 @@ add_external_project(netcdf4
   DEPENDS hdf5 zlib szip
   BUILD_IN_SOURCE 0
   PATCH_COMMAND
-    ${CMAKE_COMMAND} -E copy_if_different
-        "${SuperBuild_PROJECTS_DIR}/patches/nc-config.in.cmake"
-        "<SOURCE_DIR>/nc-config.in.cmake"
+    "${CMAKE_COMMAND}"
+      "-DPATCHES_DIR:PATH=${SuperBuild_PROJECTS_DIR}/patches/"
+      "-DSOURCE_DIR:PATH=<SOURCE_DIR>"
+      "-DINSTALL_DIR:PATH=<INSTALL_DIR>"
+      -P "${SuperBuild_PROJECTS_DIR}/netcdf4.patch.cmake"
   CMAKE_ARGS
     -DENABLE_NETCDF_4:BOOL=TRUE
     -DHDF5_DIR:PATH=<INSTALL_DIR>
---
+++ Projects/netcdf4.patch.cmake
@@ -0,0 +1,12 @@
+if(WIN32)
+    ${CMAKE_COMMAND} -E copy_if_different
+        "${PATCHES_DIR}/netcdf4.cmakelists.txt"
+        "${SOURCE_DIR}/CMakeLists.txt"
+endif()
+
+execute_process(COMMAND
+    ${CMAKE_COMMAND} -E copy_if_different
+        "${PATCHES_DIR}/nc-config.in.cmake"
+        "${SOURCE_DIR}/nc-config.in.cmake"
+)
+
