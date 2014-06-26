2b3c75285a4996f6769c22585c90c2350208135e
Ben Boeckel <ben.boeckel@kitware.com>
2014-06-26 14:49:55 -0400

    python: use a cmake-ified python which supports static builds
    Change-Id: I7c7c8262e5ba03b1dbe6bff14ce84f0c7ba5dac2

--- Projects/python.cmake
+++ Projects/python.cmake
@@ -2,17 +2,42 @@ if (APPLE)
   message(FATAL_ERROR "ABORT")
 endif()
 
-set(libtype "--enable-shared")
+set(shared ${BUILD_SHARED_LIBS})
+if (BUILD_SHARED_LIBS)
+  set(static OFF)
+else ()
+  set(static ON)
+endif ()
 if (CROSS_BUILD_STAGE STREQUAL "TOOLS")
-  set(libtype "--enable-static --disable-shared")
+  set(shared OFF)
+  set(static ON)
 endif()
+
+set(extra_args)
+if (static)
+  set(extra_args
+    -DWITH_THREAD:BOOL=OFF
+    -DPYTHON_ALL_BUILTIN:BOOL=ON
+    -DENABLE_CURSES:BOOL=OFF
+    -DENABLE_CURSES_PANEL:BOOL=OFF
+    -DENABLE_MULTIPROCESSING:BOOL=OFF
+    -DENABLE_TKINTER:BOOL=OFF
+    -DBUILTIN_CRYPT:BOOL=OFF
+    -DBUILTIN_CTYPES:BOOL=OFF
+    -DBUILTIN_ELEMENTTREE:BOOL=OFF
+    -DBUILTIN_EXPAT:BOOL=OFF
+    -DBUILTIN_NIS:BOOL=OFF
+    -DBUILTIN_PYEXPAT:BOOL=OFF
+    -DBUILTIN_TESTCAPI:BOOL=OFF)
+endif ()
 
 add_external_project_or_use_system(python
   DEPENDS bzip2 zlib png
-  CONFIGURE_COMMAND <SOURCE_DIR>/configure
-                    --prefix=<INSTALL_DIR>
-                    --enable-unicode
-                    ${libtype}
+  CMAKE_ARGS
+    -DBUILD_SHARED:BOOL=${shared}
+    -DBUILD_STATIC:BOOL=${static}
+    -DPy_USING_UNICODE:BOOL=ON
+    ${extra_args}
   )
 if (NOT CROSS_BUILD_STAGE STREQUAL "CROSS")
   # Pass the -rpath flag when building python to avoid issues running the
--- versions.cmake
+++ versions.cmake
@@ -60,15 +60,9 @@ add_revision(qt
   URL "http://paraview.org/files/dependencies/qt-everywhere-opensource-src-4.8.6.tar.gz"
   URL_MD5 2edbe4d6c2eff33ef91732602f3518eb)
 
-if (WIN32 OR (CROSS_BUILD_STAGE STREQUAL "CROSS"))
-  add_revision(python
-    URL "http://www.paraview.org/files/dependencies/Python-2.7.3.tgz"
-    URL_MD5 "2cf641732ac23b18d139be077bd906cd")
-else()
-  add_revision(python
-    URL "http://paraview.org/files/dependencies/Python-2.7.2.tgz"
-    URL_MD5 "0ddfe265f1b3d0a8c2459f5bf66894c7")
-endif()
+add_revision(python
+  GIT_REPOSITORY "https://github.com/mathstuf/cpython"
+  GIT_TAG "cmake/2.7.3")
 
 add_revision(numpy
   URL "http://paraview.org/files/dependencies/numpy-1.8.1+cmake+static.tar.bz2"
